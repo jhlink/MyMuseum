4f183ccd34e0ad0ff924f65d3e74593a39b09c59
James Kim <james@firstbuild.com>
2017-10-31 21:02:02 -0400

    feat: Create Museum logic script with movement

---
+++ NE-LFS-Museum/Assets/Scripts/MuseumLogic.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MuseumLogic : MonoBehaviour {
+	
+	public GameObject player;
+	public GameObject eventSystem;
+
+	public float height = 2;
+	public float maxMoveDistance = 10;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void Move(GameObject waypoint) {
+		Debug.Log ("End of puzzle display");
+
+		iTween.MoveTo (player, 
+			iTween.Hash (
+				"position", new Vector3 (waypoint.transform.position.x, waypoint.transform.position.y + height / 2, waypoint.transform.position.z), 
+				"time", 3F, 
+				"easetype", "linear"
+			)
+		);
+	}
+		
+		
+
+
+}
